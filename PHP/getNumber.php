c183ce5f00b37dc1370435934198e1ccf09d2183
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-02-09 11:20:04 +0200

    add: first API getNumber in php

---
+++ PHP/getNumber.php
@@ -0,0 +1,16 @@
+<?php 
+
+$x = (int)$_GET['x'];
+$y = (int)$_GET['y'];
+
+function calcNumbers($a,$b) {
+    return 2 * $a + $b % 2;
+}
+
+$z = calcNumbers($x, $y);
+
+$number = array('number' => $z);
+
+echo json_encode($number);
+
+?>
