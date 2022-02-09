be850b7ef8c7f7a38ecfc7418d505b47bd919931
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-02-09 12:34:12 +0200

    add: js function to fetch data from palindrome api

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,17 @@
 // let API_number_link = "http://localhost/PHP-project/PHP/getNumber.php?x=&y=";
-let API_palindrom_link = "";
+let API_palindrom_link = "http://localhost/PHP-project/PHP/palindrome.php?string=";
 let API_spaces_link = "";
 let API_password_link = "";
 
 document.getElementById("submit_number").addEventListener("click", getNumber);
+document.getElementById("submit_string").addEventListener("click", getString);
+
+function getString() {
+    let str = document.getElementById("string").value;
+    let API_palindrom_link = `http://localhost/PHP-project/PHP/palindrome.php?string=${str}`;
+
+    fetchData(API_palindrom_link, "string");
+}
 
 function getNumber() {
     let x = document.getElementById("first_number").value;
@@ -20,11 +28,17 @@ async function fetchData(url, data_type) {
 
     if (data_type === "number") {
         renderNumber(data);
+    }else if (data_type === "string") {
+        renderString(data);
     }
 }
 
 function renderNumber(data_number) {
     console.log(data_number);
+}
+
+function renderString (data_string) {
+    console.log(data_string);
 }
 
 
